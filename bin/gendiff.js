63a11d441030381e8141565c44ebd91344a6f170
Vladimir Gudkov <vgudkov777@gmail.com>
2023-11-21 00:23:32 +0700

    3 step is done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,8 +6,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('filepath1')
-  .argument('filepath2')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+  });
 
 program.parse(process.argv);
---
+++ src/utils/parser.js
@@ -0,0 +1,1 @@
+export default (fileData) => JSON.parse(fileData);
